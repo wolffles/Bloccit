63e78acb7798409ecf8a9dca81eff377f5dcd63f
wolffles <tofukillzstevent@live.com>
2017-07-03 17:53:12 -0700

    assignment18 #1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each_with_index{|ele,i|
+      ele.title = "Spam" if i % 5 == 0
+    }
   end
 
   def show
