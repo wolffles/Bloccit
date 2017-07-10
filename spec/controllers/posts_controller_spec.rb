632fe851287ace061e791a7a9955f04f8a668936
wolffles <tofukillzstevent@live.com>
2017-07-10 13:47:31 -0700

    checkpoint20 #2

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe PostsController, type: :controller do
     it "assigns [my_post] to @posts" do
       get :index
 
-    expect(assigns(:posts)).to eq([my_posts])
+    expect(assigns(:posts)).to eq([my_post])
     end
   end
 
@@ -123,6 +123,6 @@ RSpec.describe PostsController, type: :controller do
      end
    end
 
-   
+
 
 end
