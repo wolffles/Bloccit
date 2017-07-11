e548a6b258b91a4897947b28dcbdd311361a26e3
wolffles <tofukillzstevent@live.com>
2017-07-10 23:15:09 -0700

    checkpoint22 #1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,18 @@
 require 'random_data'
 
+15.times do
+  Topic.create!(
+    name: RandomData.random_sentence,
+    description: RandomData.random_paragraph
+  )
+end
+topics = Topic.all
+
  50.times do
    Post.create!(
-     title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph
+    topic: topics.sample,
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
    )
  end
  posts = Post.all
@@ -16,5 +25,6 @@ require 'random_data'
  end
 
  puts "Seed finished"
+ puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
