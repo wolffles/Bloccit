68c78f81b0e964495f34e1942ef30a154833d5a6
wolffles <tofukillzstevent@live.com>
2017-07-27 09:00:06 -0700

    checkpoint26 #1

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
  resources :users, only: [:new, :create]
 
+ resources :sessions, only: [:new, :create, :destroy]
+
  post 'users/confirm' => 'users#confirm'
 
  get 'about' => 'welcome#about'
