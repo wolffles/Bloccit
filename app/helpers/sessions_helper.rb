2403013b5d5c2d237a426c9d78a1017db2d354c1
wolffles <tofukillzstevent@live.com>
2017-07-27 10:13:46 -0700

    assignment26 #1

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,9 +10,8 @@ module SessionsHelper
   def current_user
     User.find_by(id: session[:user_id])
   end
-end
-
-def avatar_url(user)
-  gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-  "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+  end
 end
