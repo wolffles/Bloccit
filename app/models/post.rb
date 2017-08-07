464b18e002259247922885c31a97d385b86c19a1
wolffles <tofukillzstevent@live.com>
2017-08-06 20:15:38 -0700

    checkpoint30

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,9 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
 
-  default_scope { order('created_at DESC') }
+  has_many :votes, dependent: :destroy
+
+  default_scope { order('rank DESC') }
 
   scope :ordered_by_title, -> { order('title ASC') }
   scope :ordered_by_reverse_created_at, -> { order('created_at ASC')}
@@ -13,5 +15,24 @@ class Post < ActiveRecord::Base
   validates :topic, presence: true
   validates :user, presence: true
 
+  def up_votes
+    #
+    votes.where(value: 1).count
+  end
 
+  def down_votes
+    #
+    votes.where(value:-1).count
+  end
+
+  def points
+    #
+    votes.sum(:value)
+  end
+
+  def update_rank
+    age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
+    new_rank = points + age_in_days
+    update_attribute(:rank, new_rank)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,13 +18,18 @@ end
 topics = Topic.all
 
  50.times do
-   Post.create!(
+   post = Post.create!(
     user: users.sample,
     topic: topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
    )
+
+   post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+   #
+  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
  end
+
 
 25.times do
   SponseredPost.create!(
@@ -71,3 +76,4 @@ end
  puts "#{SponseredPost.count} sponsored posts created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+ puts "#{Vote.count} votes created"
